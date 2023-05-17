d35480eb0944d2b1380dfc4a6b235371dc899f9e
Anastasiia Nemyrovska <anastasiia.nemyrovsk@bell.ca>
2023-05-17 10:37:22 -0400

    users tests update

--- DRFtutorial/users/tests.py
+++ DRFtutorial/users/tests.py
@@ -17,3 +17,20 @@ class UserLoginTestCase(base_test.NewUserTestCase):
     def tearDown(self) -> None:
         self.client.logout()
         super().tearDown()
+
+
+class LoginTokenVerifyTest(base_test.NewUserTestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+
+    def test_user_login_token_verify(self):
+        client = APIClient()
+        login_response = client.post('/login/', {"email": self.email, "password": self.password}, format="json")
+        token_verify_response = client.post('/verify-token/', {"token": login_response.json()['access']}, format="json")
+        self.assertEquals(token_verify_response.status_code, 200)
+
+    def tearDown(self) -> None:
+        self.client.logout()
+        super().tearDown()
+
